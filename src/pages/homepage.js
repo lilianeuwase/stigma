92594adc90c25518fb238a75106cbe4267625c25
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2023-04-15 15:03:22 +0200

    15-Apr

---
+++ src/pages/homepage.js
@@ -0,0 +1,19 @@
+
+import Hero from '../components/hero'
+import Icons from '../components/icons'
+import Stats2 from '../components/stats2'
+
+
+export default function HomePage() {
+
+  return (
+    <div>
+
+    <Hero/>
+    <Icons/>
+    {/* <Stats/> */}
+    <Stats2/>
+     
+    </div>
+  )
+}
