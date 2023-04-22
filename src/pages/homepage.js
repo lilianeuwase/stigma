611aa8808e6cda4ff223cc2930f1ceefe038abee
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2023-04-22 17:53:40 +0200

    22-Apr

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,19 +1,14 @@
-
-import HeroTemp from '../components/herotemp'
-import Hero from '../components/home/hero'
-import Icons from '../components/home/icons'
-import Stats2 from '../components/home/stats2'
-
+import HeroTemp from "../components/herotemp";
+import Hero from "../components/home/hero";
+import Icons from "../components/home/icons";
+import Stats2 from "../components/home/stats2";
 
 export default function HomePage() {
-
   return (
     <div>
-
-    <Hero/>
-    <Icons/>
-    <Stats2/>
-    <HeroTemp/>
+      <Hero />
+      <Icons />
+      <Stats2 />
     </div>
-  )
+  );
 }
