4179ae22a564a9a58f2269dad5812e2eb07070bf
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2023-05-01 17:41:14 +0200

    1-May

--- src/App.js
+++ src/App.js
@@ -8,20 +8,26 @@ import Footer from "./components/footer";
 import Partnership from "./pages/partnership";
 import ImpactModel from "./pages/impactModel";
 import HeroTemp from "./components/herotemp";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
     <Navbar/>
+    <ScrollToTop>
       <div className="App">
         <Routes>
+        
           <Route path="/" element={<HomePage />} />
           <Route path="/impactmodel" element={<ImpactModel />} />
           <Route path="/partnership" element={<Partnership />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/view" element={<HeroTemp />} />
+         
         </Routes>
+       
       </div>
+      </ScrollToTop>
       <Footer/>
     </Router>
   );
